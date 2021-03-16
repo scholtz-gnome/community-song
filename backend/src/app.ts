2ad4d8943e4c95479c4eda732ab036df41c170a3
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-16 11:17:40 +0200

    uninstalled body-parser package and used express middleware for json and urlencoded

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, Express } from "express";
 import cors from "cors";
-import * as bodyParser from "body-parser";
 
 export function newApp(): Express {
   const app = express();
 
   app.use(cors());
-  app.use(bodyParser.json());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   app.get("/", (_, res: Response) => {
     res.send("Hello from the backend!");

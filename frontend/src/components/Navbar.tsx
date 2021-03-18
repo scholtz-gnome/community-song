c9bd6005386401ecc08d27f3629b6bf33d94b3a5
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-18 10:32:45 +0200

    edited navbar links for React app

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar: React.FC<NavbarProps> = () => {
     <nav className="navbar">
       <h1>Community Song</h1>
       <div className="links">
-        <a href="http://localhost:4000/">Home</a>
-        <a href="http://localhost:4000/create">Create</a>
+        <a href="http://localhost:3000/">Home</a>
+        <a href="http://localhost:3000/upload">Upload</a>
       </div>
     </nav>
   );

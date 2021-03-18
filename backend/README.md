b45fa7019a1a369e25e5058deb383fc898844fdf
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-18 12:15:28 +0200

    removed backend directory

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Backend
-
-The backend for Community Song is written in TypeScript and runs in Node.js using the Express.js framework.
--- backend/db/db.connection.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "../config";
-const knexfile = require("../knexfile");
-import knex from "knex";
-const environment = config.NODE_ENV || "development";
-const dbConnection = knexfile[environment];
-
-export default knex(dbConnection);

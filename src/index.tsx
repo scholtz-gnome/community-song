580a49e44e93e84d93268f91a1a9536391dcde3d
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-18 15:39:58 +0200

    removed gh-pages and its config

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,14 @@ import "./index.css";
 import App from "./App";
 import { config } from "./config";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
-    <Auth0Provider
-      domain={config.domain}
-      clientId={config.clientId}
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
-    ,
-  </HashRouter>,
+  <Auth0Provider
+    domain={config.domain}
+    clientId={config.clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById("root")
 );

436ea896136b00c1d9a6947fbab4b5ad61ace2de
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-18 14:25:26 +0200

    added ReactBrowser to index file

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import "./index.css";
 import App from "./App";
 import { config } from "./config";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <Auth0Provider
-    domain={config.domain}
-    clientId={config.clientId}
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  <BrowserRouter>
+    <Auth0Provider
+      domain={config.domain}
+      clientId={config.clientId}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+    ,
+  </BrowserRouter>,
   document.getElementById("root")
 );

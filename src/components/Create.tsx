bab8be29a4d3799d1f00d69aa933aafb4382ae80
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-19 12:48:28 +0200

    started working on form to upload a song

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,10 +1,25 @@
+import "./Create.css";
 export interface CreateProps {}
 
 const Create: React.FC<CreateProps> = () => {
   return (
-    <div>
+    <div className="create">
       <h1>Create</h1>
       <h3>Create a song and upload it to community song</h3>
+      <form>
+        <div>
+          <label>Song name</label>
+          <input type="text" required />
+        </div>
+        <div>
+          <label>Artist</label>
+          <input type="text" required />
+        </div>
+        <div>
+          <label>Upload</label>
+          <input type="file" required />
+        </div>
+      </form>
     </div>
   );
 };

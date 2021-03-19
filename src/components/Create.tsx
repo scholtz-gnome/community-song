c60526466b9f14db3a8d66bd5bad062ac44570f4
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-19 13:19:25 +0200

    handled state on create form

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,19 +1,33 @@
+import { useState } from "react";
 import "./Create.css";
 export interface CreateProps {}
 
 const Create: React.FC<CreateProps> = () => {
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+
   return (
     <div className="create">
       <h1>Create</h1>
       <h3>Create a song and upload it to community song</h3>
       <form>
         <div>
-          <label>Song name</label>
-          <input type="text" required />
+          <label>Song title</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
           <label>Artist</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          />
         </div>
         <div>
           <label>Upload</label>

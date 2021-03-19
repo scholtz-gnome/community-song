222ed1fd5aa47a08ae2f508480c6816d6a245c95
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-19 12:13:52 +0200

    added basic styles to navbar and app-header

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,12 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "./LoginButton.css";
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
   if (!isAuthenticated) {
     return (
-      !isAuthenticated && (
-        <button onClick={() => loginWithRedirect()}>Log In</button>
-      )
+      !isAuthenticated && <p onClick={() => loginWithRedirect()}>Log In</p>
     );
   } else {
     return <div></div>;

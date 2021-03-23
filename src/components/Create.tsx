eb3aad92870f8620217d479fdf5dbd51745518a4
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-03-23 17:33:02 +0200

    create upload file

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -5,33 +5,56 @@ export interface CreateProps {}
 const Create: React.FC<CreateProps> = () => {
   const [title, setTitle] = useState("");
   const [artist, setArtist] = useState("");
+  const [song, setSong] = useState("");
+
+  const onTitleChange = (e: any) => {
+    setTitle(e.target.value);
+  };
+
+  const onArtistChange = (e: any) => {
+    setArtist(e.target.value);
+  };
+
+  const onSongChange = (e: any) => {
+    setSong(e.target.files[0]);
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("artist", artist);
+    formData.append("file", song);
+
+    const res = await fetch("http://localhost:4000/song/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const json = await res.json();
+    console.log(json);
+  };
 
   return (
     <div className="create">
       <h1>Create</h1>
       <h3>Create a song and upload it to community song</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div>
           <label>Song title</label>
-          <input
-            type="text"
-            required
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+          <input type="text" name="title" required onChange={onTitleChange} />
         </div>
         <div>
           <label>Artist</label>
-          <input
-            type="text"
-            required
-            value={artist}
-            onChange={(e) => setArtist(e.target.value)}
-          />
+          <input type="text" name="artist" required onChange={onArtistChange} />
         </div>
         <div>
-          <label>Upload</label>
-          <input type="file" required />
+          <label>Song</label>
+          <input type="file" name="song" required onChange={onSongChange} />
+        </div>
+        <div>
+          <button>Create</button>
         </div>
       </form>
     </div>

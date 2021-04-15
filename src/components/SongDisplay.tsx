ee2b7260d07b0360f8ea66aa102fc501ce701398
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-04-15 09:04:06 +0200

    Display song title and artist in header

--- src/components/SongDisplay.tsx
+++ src/components/SongDisplay.tsx
@@ -34,52 +34,58 @@ const SongDisplay: React.FC = () => {
   }, []);
 
   return (
-    <div className="outline">
-      {song && (
-        <div className="song">
-          <div className="songInfo">
-            <p>Title: {song.title}</p>
-            <p>Artist: {song.artist}</p>
-            <p>Added by: {song.first_name}</p>
-            <img src={song.profile_pic} alt={song.first_name} />
-            <p>
-              Page: {pageNumber} of {numPages}
-            </p>
-            <div className="buttons">
-              <button
-                className="button"
-                onClick={() =>
-                  pageNumber > 1
-                    ? setPageNumber(pageNumber - 1)
-                    : setPageNumber(1)
-                }
-              >
-                -
-              </button>
-              <button
-                className="button"
-                onClick={() =>
-                  pageNumber < numPages
-                    ? setPageNumber(pageNumber + 1)
-                    : setPageNumber(numPages)
-                }
-              >
-                +
-              </button>
+    <div>
+      <header>
+        <h1>{song?.title}</h1>
+        <h3>{song?.artist}</h3>
+      </header>
+      <main className="outline">
+        {song && (
+          <div className="song">
+            <div className="songInfo">
+              <p>Title: {song.title}</p>
+              <p>Artist: {song.artist}</p>
+              <p>Added by: {song.first_name}</p>
+              <img src={song.profile_pic} alt={song.first_name} />
+              <p>
+                Page: {pageNumber} of {numPages}
+              </p>
+              <div className="buttons">
+                <button
+                  className="button"
+                  onClick={() =>
+                    pageNumber > 1
+                      ? setPageNumber(pageNumber - 1)
+                      : setPageNumber(1)
+                  }
+                >
+                  -
+                </button>
+                <button
+                  className="button"
+                  onClick={() =>
+                    pageNumber < numPages
+                      ? setPageNumber(pageNumber + 1)
+                      : setPageNumber(numPages)
+                  }
+                >
+                  +
+                </button>
+              </div>
             </div>
+            <Document
+              file={`data:application/pdf;base64,${song.file.toString()}`}
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+              <Page
+                pageNumber={pageNumber}
+                renderAnnotationLayer={false}
+                loading={<div className="loading"></div>}
+              />
+            </Document>
           </div>
-          <Document
-            file={`data:application/pdf;base64,${song.file.toString()}`}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            <Page
-              pageNumber={pageNumber}
-              renderAnnotationLayer={false}
-              loading={<div className="loading"></div>}
-            />
-          </Document>
-        </div>
-      )}
+        )}
+      </main>
     </div>
   );
 };
